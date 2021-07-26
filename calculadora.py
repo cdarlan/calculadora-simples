7159c054ecbe952022addf569d5177ff5063abed
Cristian <cdarlan@gmail.com>
2021-07-25 22:03:15 -0300

    init project

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+a=input('Digite o primeiro numero: ')
+b=input('Digite o segundo numero: ')
+
+#adicao
+resultado = a + b
+
+
+#subtracao
+resultado = a + b
+
+
+#multiplicacao
+resultado = a + b
+
+
+#divisao
+resultado = a + b
